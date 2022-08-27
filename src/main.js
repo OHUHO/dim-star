c5ee8e88ff6a96ae60417486dbdb6bbf01c46a44
JingChao <jc.jingchao@qq.com>
2022-08-27 21:02:13 +0800

    add player.scss

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-////import videojs from "video.js"
-//import 'video.js/dist/video-js.css'
+
+import Video from 'video.js'
+import "video.js/dist/video-js.css"
+import "videojs-flash"
+import "./assets/css/player.scss"
+import video_zhCN from 'video.js/dist/lang/zh-CN.json'
+
+
+
+Video.addLanguage('zh_CN',video_zhCN)
 
 createApp(App).use(router).mount('#app')
